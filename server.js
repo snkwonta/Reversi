add9d08f0b3097ffe9843fa8c2dcad698e98c442
Sody Nkwonta <sodienye.nkwonta@ucalgary.ca>
2020-04-13 13:59:10 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -784,19 +784,34 @@ function send_game_update(socket, game_id, message){
 
     var row, column;
     var count = 0;
+    var black = 0;
+    var white = 0;
     for(row = 0; row < 8; row++){
         for(column = 0; column < 8; column++){
-            if(games[game_id].board[row][column] != ' '){
+            if(games[game_id].legal_moves[row][column] != ' '){
                 count++;
+            }
+            if(games[game_id].board[row][column] === 'b'){
+                black++;
+            }
+            if(games[game_id].board[row][column] === 'w'){
+                white++;
             }
         }
     }
 
-    if(count == 64){
+    if(count == 0){
+        var winner = 'tie game';
+        if(black > white){
+            winner = 'black '
+        }
+        if(white > black){
+            winner='white '
+        }
         var success_data = {
             result: 'success',
             game: games[game_id],
-            who_won: 'everyone',
+            who_won: winner,
             game_id: game_id
         };
 
